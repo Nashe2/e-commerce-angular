7273bb110722aa47d7a85296b477e60d9a1f098e
Nashe Osorio <nasheog2@gmail.com>
2024-05-22 11:17:28 -0600

    Usando el metodo ngAfterViewInit.
    - En la vista counter hago una refrerencia al audio a usar.
    
    - Cree el elemento WaveSurfer obteniendo la url sin inicializar con la directiva input, y un container desde la referencia con la directiva ViewChild tipandola como elemento de referencia, siendo un elemento nativo.
    
    - El counter component  preparacion para el usar el ngOnDestroy

--- src/app/dominios/info/componentes/wave-audio/wave-audio.component.ts
+++ src/app/dominios/info/componentes/wave-audio/wave-audio.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, AfterViewInit } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
 
 @Component({
   selector: 'app-wave-audio',
   standalone: true,
   imports: [],
   templateUrl: './wave-audio.component.html',
-  styleUrl: './wave-audio.component.scss'
+  styleUrl: './wave-audio.component.scss',
 })
 export class WaveAudioComponent {
+  /* A traves de un input se mandara el audio */
+  /* el ! indica que no esta inicializada la variable y que no me arroje error, que que más adelante será inicializada */
+  @Input({ required: true }) audioUrl!: string;
+  @ViewChild('wave') container!: ElementRef;
 
+  ngAfterViewInit(): void {
+    //la referencia de container se obtiene con el decorador @ViewChild
+    WaveSurfer.create({
+      url: this.audioUrl,
+      container: this.container.nativeElement,
+    });
+  }
 }
+/* este audio se utiliza en la vista about, es decir el componente wave se usa en la vista about */
--- src/app/dominios/shared/componentes/counter/counter.component.ts
+++ src/app/dominios/shared/componentes/counter/counter.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  SimpleChanges,
+  OnChanges,
+  signal,
+} from '@angular/core';
 import { AboutComponent } from '../../../info/pages/about/about.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AboutComponent ],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss',
 })
 export class CounterComponent implements OnChanges {
   @Input({ required: true }) message: string = '';
   @Input({ required: true }) duration: number = 0;
+  //counter = signal(0); //importe signal
+  //counterRef: number | undefined;
 
   constructor() {
     console.log('soy el constructor');
@@ -23,7 +32,7 @@ export class CounterComponent implements OnChanges {
     console.log(changes);
 
     const duration = changes['duration'];
-    if(duration){
+    if (duration && duration.currentValue !== duration.previousValue) {
       this.doSomething();
     }
   }
@@ -33,19 +42,25 @@ export class CounterComponent implements OnChanges {
     console.log('-'.repeat(10));
     console.log('duration =>', this.duration);
     console.log('message =>', this.message);
+    //this.counterRef = window.setInterval(() => {
+      //this.counter.update((statePrev) => statePrev + 1);
+    //}, 1000);
   }
 
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit');
     console.log('-'.repeat(10));
+
   }
 
   ngOnDestroy() {
     console.log('ngOnDestroy');
+    //window.clearInterval(this.counterRef);
   }
 
   /* Metodo que se va a ejecutar cada vez que el input cambie */
   doSomething() {
     console.log('change duration');
+    //asyn
   }
 }
