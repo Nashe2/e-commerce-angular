fa7ad011ef1a078c99e190db30657e4505d2288a
Nashe Osorio <nasheog2@gmail.com>
2024-05-29 11:57:20 -0600

    Implementacion del header en una ruta independiente.
    - Se agregaron estilos, etiquetas html y css.
    
    - Nota: para ver los productos por el momento no debe verse el header en el template del padre.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './dominios/productos/pages/list/list.component';
 import { AboutComponent } from './dominios/info/pages/about/about.component';
-//import { HeaderComponent } from './dominios/shared/componentes/header/header.component';
+import { HeaderComponent } from './dominios/shared/componentes/header/header.component';
 
 export const routes: Routes = [
   {
@@ -12,8 +12,8 @@ export const routes: Routes = [
     path: 'about',
     component: AboutComponent
   },
-  /* {
+  {
     path: 'header',
     component: HeaderComponent
-  } */
+  }
 ];
--- src/app/dominios/shared/componentes/header/header.component.ts
+++ src/app/dominios/shared/componentes/header/header.component.ts
@@ -1,15 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+/* export class HeaderComponent {
+  hideSideMenu = signal(true);
+  @Input() cart: ProductoComponent[] = [];
 
+  toogleSideMenu() {
+    this.hideSideMenu.update((prevState) => !prevState);
+  }
+} */
+export class HeaderComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+
+  fillerContent = Array.from(
+    { length: 50 },
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  );
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
