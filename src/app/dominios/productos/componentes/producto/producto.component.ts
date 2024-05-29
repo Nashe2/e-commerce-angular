0d64650ff0a20cfa275187e7f415540602dbb6ba
Nashe Osorio <nasheog2@gmail.com>
2024-05-29 11:48:10 -0600

    Componente Producto. Comunicacion padre (list) - hijo (producto).
    - Cree una directiva Input para enviar los datos del padre al hijo, tipado como objeto.
    
    - Actualice el template de list (padre), es decir, estaba como atributos y ahora lo pase como objeto.
    
    - Reutiice el array de producto declarado como vacio en el padre.
    
    - Actualizacion del template del hijo para recibir los datos del objeto como objeto.
    
    - Tambien cambie la funcion addToHandler  del hijo (producto) del evento click y emiti el objeto producto.
    
    - Cree el signal cart como array para almacenar los productos.
    
    - Cree una directiva output para el evento que tendra el template del padre, con su respectiva funcion de actualizacion del objeto.
    
    - Cree la funcion addToCart para actualizar los objetos que tendra el carrito.
    
    - Implementacion de pipes, los cuales se importan al component.ts.
    
    - Cree importaciones cortas con Typescript (paths) en tsconfig.json.

--- src/app/dominios/productos/componentes/producto/producto.component.ts
+++ src/app/dominios/productos/componentes/producto/producto.component.ts
@@ -1,13 +1,15 @@
-import { CurrencyPipe } from '@angular/common';
+//pipes
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 /* Ejemplo de componentes material angular */
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { Producto } from '@models/producto.model';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [ReactiveFormsModule, MatButtonModule, CurrencyPipe], // ReactiveFormsModule, MatCardModule, MatButtonModule
+  imports: [ReactiveFormsModule, MatButtonModule, CurrencyPipe, UpperCasePipe], // ReactiveFormsModule, MatCardModule, MatButtonModule
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.scss',
 })
@@ -16,14 +18,14 @@ export class ProductoComponent {
   //Ahora usaremos la directiva input
   //Hijo
 
-  @Input() img: string = '';
+  /* @Input() img: string = '';
   @Input({ required: true }) title: string = '';
-  @Input({ required: true }) price: number = 0;
+  @Input({ required: true }) price: number = 0; */
 
-  @Output() addToCart = new EventEmitter();
+  @Input({ required: true }) producto!: Producto;
+  @Output() addToCartEvent = new EventEmitter<Producto>();
 
   addToCartHandler() {
-    console.log('click form child');
-    this.addToCart.emit('hola es un mensaje desde el hijo' + this.title);
+    this.addToCartEvent.emit(this.producto);
   }
 }
--- src/app/dominios/productos/pages/list/list.component.ts
+++ src/app/dominios/productos/pages/list/list.component.ts
@@ -1,34 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ProductoComponent } from './../../componentes/producto/producto.component';
-import { Producto } from '../../../../models/producto.model';
+import { Producto } from '@models/producto.model';
+import { HeaderComponent } from '@shared/componentes/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductoComponent],
+  imports: [CommonModule, ProductoComponent, HeaderComponent], //HeaderComponent
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent implements OnInit {
   productos: Producto[] = [];
 
+  cart = signal<Producto[]>([]);
+
   ngOnInit(): void {
     this.inicializarLista();
   }
 
-  //Padre
+  /*   //Padre
   formChild(event: Event) {
     console.log('Estamos en el padre');
     console.log(event);
-  }
+  } */
 
   inicializarLista(): void {
     const longitud = this.randomIntFromInterval(1, 20);
 
     for (let index = 0; index < longitud; index++) {
       const producto = {
-        title: `Productor ${index}`,
+        title: `Producto ${index}`,
         img: `https://picsum.photos/id/${this.randomIntFromInterval(
           1,
           50
@@ -40,10 +43,14 @@ export class ListComponent implements OnInit {
     }
   }
 
+  /* randomIntFromInterval con este metodo se puede usar un
+    rango numerico que se establece y lo utiliza aleatoriamente */
+
   randomIntFromInterval(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-}
 
-/* randomIntFromInterval con este metodo se puede usar un
-rango numerico que se establece y lo utiliza aleatoriamente */
+  addToCart(producto: Producto) {
+    this.cart.update((prevState) => [...prevState, producto]);
+  }
+}
