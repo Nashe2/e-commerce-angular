27adf5c7b0dbae7189c11eaea7f45a57205f820c
Nashe Osorio <nasheog2@gmail.com>
2024-06-27 14:03:51 -0600

    Url Params. Agregare un parametro a la ruta para filtrar por categorias.
    - Importamos la clase routerLinkWithHref y lo importamos en el list.component.ts
    
    - Para que envie el query params a la ruta, en el list.component.html usamos un roterLink y lo rederijimos con el query params y en forma de objeto le decimos: oye necesito que le envies un category_id y eso va a ser igual al id de la categoria. Tambien agrego el mat-button y su clase al listcomponent.ts.
    
    - Entonces cada vez que se haga clic sobre una categoria en la url se mostrara el query params como un signo de pregunta ?, el parametro y el numero de esa categoria. Esto sin recargar la pagina solo el query params de la categoria. Es decir que de forma dinamica esta cambiando ese query params.
    
    - Solo si el usuario quiere renderizar todas las tareas (es decir, recargar la pagina)  entonces  en el list.component.html.ts agregamos un routerLink con diagonal para decirle que es el home. Tambien agrego el mat-button y su clase al listcomponent.ts.
    
    - Entonces quita  el query params de la url de la pagina. Al darle el ALL.
    
    - Ahora lo que tenemos que lograr es leer ese categori ID de forma dinamica cada vez que cambie y hacer una peticion.
    Ese query params va a llegar a traves del Input al componente, muy similar al ID, porque en el routing ya le dijimos que cualquier parametro sea un ID. Es decir, un query params llegue por medio de inputs al component, entonces en este caso ese parámetro tambien va a llegar por medio de un input, solo que va hacer opcional, entonces tambien podria ser nulo en cualquier momento. En el list.component.ts . Para detectar ese cambio de forma reactiva y dinamica, el mejor lugar para hacerlo seria, ngOnChanges que corre una vez y cada vez que cambia el input.
    
    - Entonces de debe validar cuando viene y cuando no, es decir, cuando es undefined. Con changes: SimpleChanges podemos examinar si realmente hay un cambio en ese input.
    
    - Ahi es cuando ya podemos hacer el fectch a get Productos(). Para ello necesitamos ir a producto.service.ts y al metodo getProductos() tiparle category_id?: Como string.
    
    - Para hacerlo mas dinamico ponemos como const la url asignada a una new URL de la API, y si el categori_id existe entonces agregamos un parametro a la url como searchParams y le ponemos categoriId tal como aparece en la API y le asignamos el valor categori_id. Ahora podemos enviar la url.toString() , esto para devolver a un string.
    
    - Teniendo eso en cuenta, finalmente en el list.component.ts a la funcion privada donde especifica que al productoService llama al método getProductos() le enviamos el category_id.

--- src/app/dominios/productos/pages/list/list.component.ts
+++ src/app/dominios/productos/pages/list/list.component.ts
@@ -1,48 +1,72 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { ProductoComponent } from '@productos/componentes/producto/producto.component';
-import { Producto } from '@models/producto.model';
-import { HeaderComponent } from '@shared/componentes/header/header.component';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  inject,
+  signal,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import {
+  RouterLinkWithHref
+} from '@angular/router';
+import { Category } from '@models/category.model';
+import { Producto } from '@models/producto.model';
+import { ProductoComponent } from '@productos/componentes/producto/producto.component';
+import { HeaderComponent } from '@shared/componentes/header/header.component';
 import { CartService } from '@shared/servicios/cart.service';
-import { ProductoService } from '@shared/servicios/producto.service';
-import { Category } from '@models/categoty.model';
 import { CategoryService } from '@shared/servicios/category.service';
+import { ProductoService } from '@shared/servicios/producto.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductoComponent, HeaderComponent, MatCardModule], //HeaderComponent
+  imports: [
+    CommonModule,
+    ProductoComponent,
+    HeaderComponent,
+    MatCardModule,
+    RouterLinkWithHref,
+    MatButtonModule,
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnChanges {
 
   productos = signal<Producto[]>([]);
   categories = signal<Category[]>([]);
-    /* Inyeccion de dependecias*/
+  /* Inyeccion de dependecias*/
   private cartService = inject(CartService);
   private productoService = inject(ProductoService);
   private categoryService = inject(CategoryService);
 
+  @Input() category_id?: string;
   ngOnInit() {
-    this.getProductos();
     this.getCategories();
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getProductos();
+  }
+  /* ngOnChanges(){
+  console.log(this.category_id);
+  } */
 
   addToCart(producto: Producto) {
     this.cartService.addToCart(producto);
   }
 
   private getProductos() {
-    this.productoService.getProductos().subscribe({
+    this.productoService.getProductos(this.category_id).subscribe({
       next: (productos) => {
         this.productos.set(productos);
       },
-      error: () => {
-
-      }
-    })
+      error: () => {},
+    });
   }
 
   private getCategories() {
@@ -50,9 +74,8 @@ export class ListComponent implements OnInit {
       next: (data) => {
         this.categories.set(data);
       },
-      error: () => {
-
-      }
-    })
+      error: () => {},
+    });
   }
+
 }
--- src/app/dominios/shared/servicios/producto.service.ts
+++ src/app/dominios/shared/servicios/producto.service.ts
@@ -10,12 +10,19 @@ export class ProductoService {
   private http = inject(HttpClient);
 
   constructor() {}
+  /* getProductos() {
+    return this.http.get<Producto[]>(`https://api.escuelajs.co/api/v1/products`);
+  } */
 
-  /* Con este metodo llamamos a URL de la API.
-    Se puede tipar, en este caso será un array porque necesito un array de productos*/
-  getProductos() {
-return this.http.get<Producto[]>('https://api.escuelajs.co/api/v1/products');
+  /* Con este metodo llamamos a URL de la API.*/
+  getProductos(category_id?: string) {
+    const url = new URL(`https://api.escuelajs.co/api/v1/products/`);
+    if(category_id){
+    url.searchParams.set('categoryId', category_id)
   }
+    return this.http.get<Producto[]>(url.toString());
+  }
+
   /* Este metodo va a consumir de la API los detalles de un producto en particular donde
   concatenamos el id */
   getOne(id: string) {
